157bcf9664be83b625812602f1c3879b520445ac
Godwin VC <godwin.k@apalya.com>
2018-10-30 18:23:15 +0530

    wokring on delete course

---
+++ server/delete-course.php
@@ -0,0 +1,7 @@
+<?php
+    /*include('./connection.php');
+    $course_data = json_decode(file_get_contents('php://input'));
+    $course_id = $course_data->courseId;
+    $query = "DELETE FROM `courses` WHERE `course_id` = '$course_id'";
+    echo execute($db, $query) ? json_encode("DELETED $course_id") : json_encode('ERROR');*/
+?>
---
+++ server/delete-folder.php
@@ -0,0 +1,32 @@
+<?php
+  function deleteFolder(){
+    $dir = dirname(__DIR__).'/courses/'.$course_id;
+    $it = new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS);
+    $files = new RecursiveIteratorIterator($it, RecursiveIteratorIterator::CHILD_FIRST);
+    foreach($files as $file) {
+        if ($file->isDir()){
+          if (!@rmdir($file->getRealPath())) {
+            $error = error_get_last();
+        
+            if (preg_match('/something/', $error['message'])) {
+                return false;
+            } elseif (preg_match('/somethingelse/', $error['message'])) {
+                return false;
+            }
+        }
+            
+        } else {
+            unlink($file->getRealPath());
+        }
+    }
+    if (!@rmdir($dir)) {
+      $error = error_get_last();
+  
+      if (preg_match('/something/', $error['message'])) {
+          return false;
+      } elseif (preg_match('/somethingelse/', $error['message'])) {
+          return false;
+      }
+  }
+  return true;
+    }
